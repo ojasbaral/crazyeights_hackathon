f0fb93385af4c442b2b5b5451be96ff55625b08b
Ojas Baral <ojas.baral@gmail.com>
2024-07-26 09:57:02 -0600

    added dashboard functionality

--- server/index.js
+++ server/index.js
@@ -26,9 +26,60 @@ db.connect()
 
   app.use(bodyParser.json())
 
-  app.get('/', (req, res) => {
-    res.json({"status": true})
-  })
+app.post('/register', async (req, res) => {
+    const {email, password, firstName, lastName, address} = req.body
+    const validQuery = `SELECT * FROM users WHERE email = $1`
+    const insertQuery = `INSERT INTO users (email, password, first_name, last_name, address) VALUES ($1, $2, $3, $4, $5) RETURNING *`
+    try {
+        const valid = await db.oneOrNone(validQuery, [email])
+        if (!valid){
+            const insert = await db.oneOrNone(insertQuery, [email, password, firstName, lastName, address])
+            console.log(insert)
+            if (insert) {
+                res.json({"message": "success"})
+            } else {
+                res.json({"message": "user exists"})
+            }
+        } else {
+            res.json({"message": "user exists"})
+        }
+    } catch (e) {
+        res.status(400).json(e)
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body
+    const query = 'SELECT * FROM users WHERE email=$1'
+    try {
+        user = await db.oneOrNone(query, [email])
+        if (user){
+            if (password == user.password) {
+                return res.send({message: "success", id: user.user_id})
+            }
+        }
+        res.status(401).json({"message": "user not found or incorrect password"})
+    } catch (e) {
+        res.status(400).json(e)
+    }
+})
+
+app.post('/dashboard', async (req, res) => {
+    const {user_id, date} = req.body
+    const pastQuery = `SELECT * FROM prescriptions_users pu FULL JOIN prescriptions p ON p.prescription_id = pu._prescription_id WHERE pu._user_id=$1 and pu.end_date < $2 ORDER BY pu.end_date DESC`
+    const futureQuery = `SELECT * FROM prescriptions_users pu FULL JOIN prescriptions p ON p.prescription_id = pu._prescription_id WHERE pu._user_id=$1 and pu.start_date > $2 ORDER BY pu.start_date ASC`
+    const currentQuery = `SELECT * FROM prescriptions_users pu FULL JOIN prescriptions p ON p.prescription_id = pu._prescription_id WHERE pu._user_id=$1 and pu.start_date <= $2 AND pu.end_date >= $2 ORDER BY pu.start_date ASC`
+    try {
+        past = await db.manyOrNone(pastQuery, [user_id, date])
+        future = await db.manyOrNone(futureQuery, [user_id, date])
+        current = await db.manyOrNone(currentQuery, [user_id, date])
+        res.json({user_id: user_id, "past": past, "current": current, "future": future})
+    } catch (e) {
+        res.status(400).json({"message": "error"})
+    }
+})
+
+
 
 module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
